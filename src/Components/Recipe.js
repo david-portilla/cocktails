29f304bf31ea31f35e9dd5e13a85b12d55fec1ab
David Portilla <david_portilla@hotmail.com>
2021-04-20 17:40:56 -0500

    adding recipe information

--- src/Components/Recipe.js
+++ src/Components/Recipe.js
@@ -5,6 +5,18 @@ export const Recipe = ({recipe}) => {
     <div className="col-md-4 mb-3">
       <div className="card">
         <h2 className="card-header">{recipe.strDrink}</h2>
+        <img
+          src={recipe.strDrinkThumb}
+          alt={recipe.strDrink}
+          className="card-img-top" />
+        <div className="card-body">
+          <button
+            type="button"
+            className="btn btn-block btn-primary"
+          >
+            View recipe
+          </button>
+        </div>
       </div>
     </div>
   )
